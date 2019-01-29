260a25f42d2ff5c51e11016e8a040fcbe86cb1d8
Vincent Chee <vincent.kh.chee@gmail.com>
2019-01-28 23:06:15 -0500

    theys see me wippin

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../styles/index.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Dope bois in the building</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('hello world!');
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
+
+ReactDOM.render(<App />, document.getElementById("root"));
