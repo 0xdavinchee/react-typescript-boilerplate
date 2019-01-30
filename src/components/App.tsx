0e09dd97020e48106810ba4a46fd6373cf56e334
Vincent Chee <vincent.kh.chee@gmail.com>
2019-01-29 23:25:13 -0500

    wip
    - tslint added
    - tsconfig updated
    - more packages added

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '/styles/index.css';
+
+class App extends React.PureComponent {
+	render() {
+		return (
+			<div>
+				<h1>Dope goys in the building</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
